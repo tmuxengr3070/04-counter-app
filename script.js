3ccf2bc9abe54939377ee28d95eeb3a500fa0146
Anthony McMorris <amcmorr2@asu.edu>
2025-09-27 22:10:20 +0000

    Reset button

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const increaseButton = document.getElementById("increase"); // const because we 
 // Get the Exit button. We use const because we do not reassign this variable.
 const decreaseButton = document.getElementById("decrease");
 
+// Get the Reset button. We use const because we do not reassign this variable.
+const resetButton = document.getElementById("reset");
+
 // Use let for count because its value will change
 let count = 0;
 
@@ -21,5 +24,11 @@ decreaseButton.addEventListener("click", function() {
   if (count > 0) { // Only decrease if count is above 0
     count--; // Decrease the count by 1
   }
+  countDisplay.textContent = count; // Update the display
+});
+
+// Add a click event listener to the Reset button
+resetButton.addEventListener("click", function() {
+  count = 0; // Set the count to zero
   countDisplay.textContent = count; // Update the display
 });
