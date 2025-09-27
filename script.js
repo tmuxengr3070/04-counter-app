be18c5b9bf46939e66374d61f1684c834a919560
Anthony McMorris <amcmorr2@asu.edu>
2025-09-27 22:08:43 +0000

    updates

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ const countDisplay = document.getElementById("count"); // const because we don't
 // Get the Enter button
 const increaseButton = document.getElementById("increase"); // const because we don't reassign this variable
 
+// Get the Exit button. We use const because we do not reassign this variable.
+const decreaseButton = document.getElementById("decrease");
+
 // Use let for count because its value will change
 let count = 0;
 
 // Add a click event listener to the Enter button
 increaseButton.addEventListener("click", function() {
   count++; // Increase the count by 1
+  countDisplay.textContent = count; // Update the display
+});
+
+// Add a click event listener to the Exit button
+decreaseButton.addEventListener("click", function() {
+  if (count > 0) { // Only decrease if count is above 0
+    count--; // Decrease the count by 1
+  }
   countDisplay.textContent = count; // Update the display
 });
